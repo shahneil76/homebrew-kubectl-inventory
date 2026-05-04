9c9e31e8fe2f65a46743f5d95708db74f4d4bc65
Neil Shah <neil.shah@censhare.com>
2026-05-04 20:48:03 +0530

    chore: bump to v0.3.0 (web UI dashboard)

--- Formula/kubectl-inventory.rb
+++ Formula/kubectl-inventory.rb
@@ -4,28 +4,28 @@
 class KubectlInventory < Formula
   desc "Complete Kubernetes namespace inventory with orphan and stuck finalizer detection"
   homepage "https://github.com/shahneil76/kubectl-inventory"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/shahneil76/kubectl-inventory/releases/download/v0.2.0/kubectl-inventory_0.2.0_darwin_arm64.tar.gz"
-      sha256 "d3e749a7a1e532bc5ed065e60698cfe67be370194edf207b328aac2eead76a47"
+      url "https://github.com/shahneil76/kubectl-inventory/releases/download/v0.3.0/kubectl-inventory_0.3.0_darwin_arm64.tar.gz"
+      sha256 "15589168d5ca8c39c1854591ccde00776525ac1e18b11482a70b5513daf6c3b3"
     end
     on_intel do
-      url "https://github.com/shahneil76/kubectl-inventory/releases/download/v0.2.0/kubectl-inventory_0.2.0_darwin_amd64.tar.gz"
-      sha256 "a9b385410398fa1ce7026a33391522b290a53628489748920a1546c93ff4fdf6"
+      url "https://github.com/shahneil76/kubectl-inventory/releases/download/v0.3.0/kubectl-inventory_0.3.0_darwin_amd64.tar.gz"
+      sha256 "ce47daa986981cae2f8a5410c63c5a4de8792b2798e61a0a9865c39617ca1944"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/shahneil76/kubectl-inventory/releases/download/v0.2.0/kubectl-inventory_0.2.0_linux_arm64.tar.gz"
-      sha256 "1e9321e6d9d1629711ffd5f1bbfcdfb6d3e58d30870be9bf1e53fec195c5baef"
+      url "https://github.com/shahneil76/kubectl-inventory/releases/download/v0.3.0/kubectl-inventory_0.3.0_linux_arm64.tar.gz"
+      sha256 "fe131250273feabdcb19a6aa0f93bbeecc618660a1ed67e66dfcd227bacb2e9b"
     end
     on_intel do
-      url "https://github.com/shahneil76/kubectl-inventory/releases/download/v0.2.0/kubectl-inventory_0.2.0_linux_amd64.tar.gz"
-      sha256 "f4df4ce2f661284c5bfa5dceeee066848e3e2ced29495ad738bdd41558575e58"
+      url "https://github.com/shahneil76/kubectl-inventory/releases/download/v0.3.0/kubectl-inventory_0.3.0_linux_amd64.tar.gz"
+      sha256 "8e2200ae55b3be175e6c7d898e4333624cf0ac5a726ec65d521edb83834b3d25"
     end
   end
 
